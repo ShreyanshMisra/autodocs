c1c6d144c749a5ba90d3828c717431723201ce42
shreyansh <shreyansh@umass.edu>
2023-08-05 17:23:48 +0400

    Refactor: bug fixes

--- src/components/ui/(dash)/ApiDashboard.tsx
+++ src/components/ui/(dash)/ApiDashboard.tsx
@@ -1,32 +1,25 @@
-import { formatDistance } from "date-fns";
-import { getServerSession } from "next-auth";
-import { notFound } from "next/navigation";
-import { authOptions } from "@/lib/auth";
-import { db } from "@/lib/db";
-
-import ApiKeyOptions from "./ApiKeyOptions";
-
-import Masthead from "../Masthead";
-import Paragraph from "../Paragraph";
-import Table from "./Table";
-import { Input } from "./Input";
-
-interface ApiKey {
-  id: any;
-  enabled: boolean;
-}
+import { authOptions } from '@/lib/auth'
+import { db } from '@/lib/db'
+import { formatDistance } from 'date-fns'
+import { getServerSession } from 'next-auth'
+import { notFound } from 'next/navigation'
+import ApiKeyOptions from './ApiKeyOptions'
+import { Input } from './Input'
+import Masthead from '../Masthead'
+import Paragraph from '../Paragraph'
+import Table from './Table'
 
 const ApiDashboard = async ({}) => {
-  const user = await getServerSession(authOptions);
-  if (!user) return notFound();
+  const user = await getServerSession(authOptions)
+  if (!user) return notFound()
 
-  const apiKeys: ApiKey[] = await db.apiKey.findMany({
+  const apiKeys = await db.apiKey.findMany({
     where: { userId: user.user.id },
-  });
+  })
 
-  const activeApiKey = apiKeys.find((key) => key.enabled);
+  const activeApiKey = apiKeys.find((key) => key.enabled)
 
-  if (!activeApiKey) return notFound();
+  if (!activeApiKey) return notFound()
 
   const userRequests = await db.apiRequest.findMany({
     where: {
@@ -34,29 +27,29 @@ const ApiDashboard = async ({}) => {
         in: apiKeys.map((key) => key.id),
       },
     },
-  });
+  })
 
   const serializableRequests = userRequests.map((req) => ({
     ...req,
     timestamp: formatDistance(new Date(req.timestamp), new Date()),
-  }));
+  }))
 
   return (
-    <div className="container flex flex-col gap-6">
+    <div className='container flex flex-col gap-6'>
       <Masthead>Welcome back, {user.user.name}</Masthead>
-      <div className="flex flex-col md:flex-row gap-4 justify-center md:justify-start items-center">
+      <div className='flex flex-col md:flex-row gap-4 justify-center md:justify-start items-center'>
         <Paragraph>Your API key:</Paragraph>
-        <Input className="w-fit truncate" readOnly value={activeApiKey?.toString()} />
-        <ApiKeyOptions apiKeyKey={activeApiKey?.toString()} />
+        <Input className='w-fit truncate' readOnly value={activeApiKey.key} />
+        <ApiKeyOptions apiKeyKey={activeApiKey.key} />
       </div>
 
-      <Paragraph className="text-center md:text-left mt-4 -mb-4">
+      <Paragraph className='text-center md:text-left mt-4 -mb-4'>
         Your API history:
       </Paragraph>
 
       <Table userRequests={serializableRequests} />
     </div>
-  );
-};
+  )
+}
 
-export default ApiDashboard;
+export default ApiDashboard
